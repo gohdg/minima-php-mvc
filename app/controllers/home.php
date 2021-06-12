75caddb39d950247c670295f2e979640f4f87b69
gohdg <gohdg@naver.com>
2021-06-12 21:35:15 +0900

    db connection , youtube 1:14:04

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,6 +4,9 @@ class Home extends Controller
 {
   function index()
   {
+    $DB = new Database();
+    $DB->db_connect();
+
     $this->view("home");
   }
 }
--- app/core/database.php
+++ app/core/database.php
@@ -2,4 +2,22 @@
 
 class Database
 {
+  public function db_connect()
+  {
+    try {
+      $string = DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";";
+      return $db = new PDO($string, DB_USER, DB_PASS);
+      showLog($db);
+    } catch (PDOException $e) {
+      die($e->getMessage());
+    }
+  }
+
+  public function read($query, $data = [])
+  {
+  }
+
+  public function write($query, $data = [])
+  {
+  }
 }
